c97c6e92d0a4daf14fef84a8646c5e43c4191132
joshi <joseph.ilagan.99@gmail.com>
2023-12-18 21:55:09 +0800

    .

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useState } from 'react';
 
 function App() {
+
+  const [name, setName] = useState("")
+  const [predictedAge, setPredictedAge] = useState()
+
+  const fetchData = () => {
+    axios.get(`https://api.agify.io/?name=${name}`)
+    .then((res) => {
+      setPredictedAge(res.data)
+    })
+  }
+
   return (
     <div className="App">
-      
+      <input 
+        placeholder='Name'
+        onChange={(e) => setName(e.target.value)}
+      />
+      <button onClick={fetchData}>Predict Age</button>
+      <h1>Predicted Name: {predictedAge?.name}</h1>
+      <h1>Predicted Age: {predictedAge?.age}</h1>
+      <h1>Predicted count: {predictedAge?.count}</h1>
     </div>
   );
 }
